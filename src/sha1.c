91abdd7d487246c73f181b9ed0fe35d9028cfdb7
kaleb <kaleb.kreuer@proton.me>
2024-08-03 07:16:06 -0400

    finish sha1_step() draft

--- src/sha1.c
+++ src/sha1.c
@@ -4,6 +4,62 @@
  * Thank you Steve Reid <steve@edmweb.com> for his implementation of SHA1 in C
  */
 
+// define transformative functions
+
+uint32_t rol(uint32_t value, unsigned bits)
+{
+  return (value << bits) | (value >> (32 - bits));
+}
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+uint32_t blk0(unsigned i, CHAR64LONG16 **block)
+{
+  return (rol((*block)->l[i],24)&0xFF00FF00) | (rol((*block)->l[i], 8) & 0x00FF00FFU);
+}
+#elif BYTE_ORDER == BIG_ENDIAN
+uint32_t blk0(unsigned i, CHAR64LONG16 **block)
+{
+  return (*block)->l[i];
+}
+#else
+#error "Endianness is not defined!"
+#endif
+
+uint32_t blk(unsigned i, CHAR64LONG16 **block)
+{
+  return (*block)->l[i&15] = rol((*block)->l[(i+13)&15] ^ (*block)->l[(i+8)&15] ^ (*block)->l[(i+2)&15] ^ (*block)->l[i&15], 1);
+}
+
+void R0(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block)
+{
+  z += ((w & (x ^ y)) ^ y) + blk0(i, &block) + 0x5A827999U + rol(v, 5);
+  w = rol(w, 30);
+}
+
+void R1(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block)
+{
+  z += ((w & (x ^ y)) ^ y) + blk(i, &block) + 0x5A827999U + rol(v, 5);
+  w = rol(w, 30);
+}
+
+void R2(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block)
+{
+  z += (w ^ x ^ y) + blk(i, &block) + 0x6ED9EBA1U + rol(v, 5);
+  w = rol(w, 30);
+}
+
+void R3(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block)
+{
+  z += (((w | x) & y) | (w & x)) + blk(i, &block) + 0x8F1BBCDCU + rol(v, 5);
+  w = rol(w, 30);
+}
+
+void R4(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block)
+{
+  z += (w ^ x ^ y) + blk(i, &block) + 0xCA62C1D6U + rol(v, 5);
+  w = rol(w, 30);
+}
+
 void sha1_init(sha1_ctx *ctx)
 {
   ctx->state[0] = 0x67452301U;
@@ -18,6 +74,72 @@ void sha1_init(sha1_ctx *ctx)
 
 void sha1_step(sha1_ctx *ctx)
 {
+  uint32_t a, b, c, d, e;
+  a = b = c = d = e = 0;
+
+  CHAR64LONG16 block[1];  // def in header
+  memcpy(block, ctx->buffer, 64); // copy 64 bits of buffer into CHAR64LONG16 union
+
+  a = ctx->state[0];
+  b = ctx->state[1];
+  c = ctx->state[2];
+  d = ctx->state[3];
+  e = ctx->state[4];
+
+  // 4 rounds of 20 operations (R0+R1)
+  for (unsigned i = 0; i <= 79; i++)
+  {
+    if (i <= 15)
+    {
+      if      ((i+5) % 5 == 0) { R0(a, b, c, d, e, i, &(block[0])); } 
+      else if ((i+5) % 5 == 1) { R0(e, a, b, c, d, i, &(block[0])); }
+      else if ((i+5) % 5 == 2) { R0(d, e, a, b, c, i, &(block[0])); }
+      else if ((i+5) % 5 == 3) { R0(c, d, e, a, b, i, &(block[0])); }
+      else if ((i+5) % 5 == 4) { R0(b, c, d, e, a, i, &(block[0])); }
+    }
+    else if (i <= 19)
+    {
+      if      (i % 5 == 1) { R1(e, a, b, c, d, i, &(block[0])); }
+      else if (i % 5 == 2) { R1(d, e, a, b, c, i, &(block[0])); }
+      else if (i % 5 == 3) { R1(c, d, e, a, b, i, &(block[0])); }
+      else if (i % 5 == 4) { R1(b, c, d, e, a, i, &(block[0])); }
+    }
+    else if (i <= 39)
+    {
+      if      (i % 5 == 0) { R2(a, b, c, d, e, i, &(block[0])); }
+      else if (i % 5 == 1) { R2(e, a, b, c, d, i, &(block[0])); }
+      else if (i % 5 == 2) { R2(d, e, a, b, c, i, &(block[0])); }
+      else if (i % 5 == 3) { R2(c, d, e, a, b, i, &(block[0])); }
+      else if (i % 5 == 4) { R2(b, c, d, e, a, i, &(block[0])); }
+    }
+    else if (i <= 59)
+    {
+      if      (i % 5 == 0) { R3(a, b, c, d, e, i, &(block[0])); }
+      else if (i % 5 == 1) { R3(e, a, b, c, d, i, &(block[0])); }
+      else if (i % 5 == 2) { R3(d, e, a, b, c, i, &(block[0])); }
+      else if (i % 5 == 3) { R3(c, d, e, a, b, i, &(block[0])); }
+      else if (i % 5 == 4) { R3(b, c, d, e, a, i, &(block[0])); }
+    }
+    else if (i <= 79)
+    {
+      if      (i % 5 == 0) { R4(a, b, c, d, e, i, &(block[0])); }
+      else if (i % 5 == 1) { R4(e, a, b, c, d, i, &(block[0])); }
+      else if (i % 5 == 2) { R4(d, e, a, b, c, i, &(block[0])); }
+      else if (i % 5 == 3) { R4(c, d, e, a, b, i, &(block[0])); }
+      else if (i % 5 == 4) { R4(b, c, d, e, a, i, &(block[0])); }
+    }
+  }
+
+  // add working vars back into state
+  ctx->state[0] += a;
+  ctx->state[1] += b;
+  ctx->state[2] += c;
+  ctx->state[3] += d;
+  ctx->state[4] += e;
+
+  // wipe block
+  memset(block, '\0', sizeof(block));
+
   return;
 }
 
@@ -50,7 +172,7 @@ void sha1(FILE *stream, uint64_t stream_len, uint8_t *sha1_result)
   rewind(stream);
 
   // pass result to main
-  memcpy(sha1_result, ctx.state, 20);
+  memcpy(sha1_result, ctx.digest, 20);
   return;
 
   return;
--- src/sha1.h
+++ src/sha1.h
@@ -8,9 +8,25 @@
 typedef struct
 {
   uint32_t state[5];
+  uint8_t digest[20];
   uint32_t count[2];
   uint8_t buffer[64];
 } sha1_ctx;
+
+typedef union
+{
+  uint8_t c[64];
+  uint32_t l[16];
+} CHAR64LONG16;
+
+uint32_t rol(uint32_t value, unsigned bits);
+uint32_t blk0(unsigned i, CHAR64LONG16 **block);
+uint32_t blk(unsigned i, CHAR64LONG16 **block);
+void R0(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block);
+void R1(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block);
+void R2(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block);
+void R3(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block);
+void R4(uint32_t v, uint32_t w, uint32_t x, uint32_t y, uint32_t z, unsigned i, CHAR64LONG16 *block);
 
 void sha1(FILE *stream, uint64_t stream_len, uint8_t *sha1_result);
 void sha1_init(sha1_ctx *ctx);
